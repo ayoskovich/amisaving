9f41bdd4324c8f7b56db227d6f2ed3777a849286
anthony <yoskovia@mail.gvsu.edu>
2020-09-16 18:57:28 -0400

    Rendering latex from sympy properly for matplotlib

--- src/prototype.py
+++ src/prototype.py
@@ -26,6 +26,13 @@ def build_cost(slope, inter=0):
     return COST.subs(m, slope).subs(b, inter)
 
 
+def convert_title(title):
+    """ Convert a sympy latex string for
+    use in matplotlib
+    """
+    return "$" + title + "$"
+
+
 # In[ ]:
 
 
@@ -47,8 +54,8 @@ x_vals = np.linspace(EQ-BUF, EQ+BUF, 100)
 y_vals1 = lam_a(x_vals)
 y_vals2 = lam_b(x_vals)
 
-plt.plot(x_vals, y_vals1, label='With equipment: '  + latex(fixed));
-plt.plot(x_vals, y_vals2, label='Buying each day: ' + latex(variable));
+plt.plot(x_vals, y_vals1, label='With equipment: '  + convert_title(latex(fixed)));
+plt.plot(x_vals, y_vals2, label='Buying each day: ' + convert_title(latex(variable)));
 
 plt.axvline(x=EQ, color='green');
 
@@ -67,3 +74,43 @@ plt.legend();
 # 
 # - How many frequency units until they're making money?
 # - \[Yes / No\] will you save money if you use this once a day for 500 days?
+
+# In[ ]:
+
+
+from bokeh.plotting import figure, output_file, show, output_notebook
+
+# prepare some data
+x = [0.1, 0.5, 1.0, 1.5, 2.0, 2.5, 3.0]
+y0 = [i**2 for i in x]
+y1 = [10**i for i in x]
+y2 = [10**(i**2) for i in x]
+
+# output to static HTML file
+# output_file("log_lines.html")
+output_notebook()
+
+# create a new plot
+p = figure(
+   tools="pan,box_zoom,reset,save",
+   y_axis_type="log", y_range=[0.001, 10**11], title="log axis example",
+   x_axis_label='sections', y_axis_label='particles'
+)
+
+# add some renderers
+p.line(x, x, legend_label="y=x")
+p.circle(x, x, legend_label="y=x", fill_color="white", size=8)
+p.line(x, y0, legend_label="y=x^2", line_width=3)
+p.line(x, y1, legend_label="y=10^x", line_color="red")
+p.circle(x, y1, legend_label="y=10^x", fill_color="red", line_color="red", size=6)
+p.line(x, y2, legend_label="y=10^x^2", line_color="orange", line_dash="4 4")
+
+# show the results
+show(p)
+
+
+# In[ ]:
+
+
+
+
