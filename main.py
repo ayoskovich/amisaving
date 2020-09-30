98401efa9cf32a349dacb79147d74b61809d8d8a
Anthony <anthony.yoskovich@gmail.com>
2020-09-30 11:10:41 -0400

    Adding jupyter notebook to dev branch

--- main.py
+++ main.py
@@ -5,16 +5,20 @@ from bokeh.models import Button, TextInput
 from bokeh.palettes import RdYlBu3
 from bokeh.plotting import figure, curdoc
 
-x_vals = np.linspace(0, 10, 20)
-y_vals = x_vals*2
+# x_vals = np.linspace(0, 10, 20)
+x_vals = np.array([0, 1, 2, 3, 4])
+y_vals = x_vals
 
+df = ColumnDataSource(data={'x':x_vals,
+                            'y':y_vals})
 
 # create a plot and style its properties
-p = figure(title="This is a graph of a thing", x_range=(0, 100), y_range=(0, 100), 
+p = figure(title="This is a graph of a thing", x_range=(0, 10), y_range=(0, 10), 
            toolbar_location="right")
 
-r = p.line(x_vals, y_vals)
+r = p.line(x='x', y='y', source=df)
 dat = r.data_source
+
 
 def callback(attr, old, new):
     new_data = dict()
@@ -31,7 +35,7 @@ def callback2(attr, old, new):
     new_data = dict()
     new_data['x'] = dat.data['x']
     try:
-        new_data['y'] = int(new) + dat.data['y']
+        new_data['y'] = int(new) + dat.data['x']
     except:
         pass
 
@@ -39,11 +43,11 @@ def callback2(attr, old, new):
 
 
 
-slope_input = TextInput(value="", title="Enter variable cost here")
+slope_input = TextInput(value="", title="Slope")
 slope_input.on_change("value", callback)
 
 
-int_input = TextInput(value="", title="Enter fixed cost here")
+int_input = TextInput(value="", title="Intercept")
 int_input.on_change("value", callback2)
 
 # put the button and plot in a layout and add to the document
