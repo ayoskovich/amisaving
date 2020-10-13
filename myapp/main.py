b77649e1183e008d5151f62bfbe36ffb720dade9
Anthony <anthony.yoskovich@gmail.com>
2020-10-13 15:04:59 -0400

    Adding more descriptive text

--- myapp/main.py
+++ myapp/main.py
@@ -31,10 +31,10 @@ p.line(x='x', y='y2', line_width=wid, color="blue", source=df)
 eq_solve = Span(location=0, dimension='height', line_width=2)
 p.add_layout(eq_solve)
 
-slope_input = TextInput(value="", title="Cost per unit with equipment")
-var_input = TextInput(value="", title="Cost per unit without equipment")
-int_input = TextInput(value="", title="Cost of equipment")
-button = Button(label="Draw!", css_classes=["my_button"])
+slope_input = TextInput(value="", title="Cost per unit with equipment:")
+var_input = TextInput(value="", title="Cost per unit without equipment:", width=PAGE_WIDTH)
+int_input = TextInput(value="", title="Cost of equipment:")
+button = Button(label="Draw!")
 
 
 def b_call(event):
@@ -63,8 +63,27 @@ def b_call(event):
 
 header = Div(text="<h1>Am I Saving?</h1>")
 description = Div(text="""
+<h3>
+Should I invest in equipment to brew coffee at home, or is it cheaper to just buy coffee at Starbucks? Should I purchase a car or Uber everywhere? This calculator is meant to help you make these types of decisions.
+</h3>
 <p>
-Should I invest in equipment to brew coffee at home? While it costs some money to start off, after a while I may be saving money. Over what time frame am I better off making each choice?
+In order to describe how to use this tool let's focus on the coffee example and let's say I have 2 options: 
+</p>
+<ol>
+  <li>Purchase equipment and brew at home.</li>
+  <li>Skip the equipment and purchase a cup of coffee at Starbucks.</li>
+</ol>
+<p>
+For option 1, I would need to purchase equipment, and let's say that costs $60. Then, once I have the equipment, for each additional cup of coffee I only need to pay for the beans. Let's say that one cup of coffee takes $1 worth of beans.
+For option 2, I spend $0 on equipment, but each cup costs me the price at Starbucks, and let's say that's $3.
+</p>
+<p>
+That's a lot of words! In summary...
+<ul>
+  <li><span class='bold'>Cost of equipment:</span> The $60 I would spend on coffee equipment (filters, coffee machine, kettle, etc.).</li>
+  <li><span class='bold'>Cost per unit with equipment:</span> The $1 I need to spend for each cup of coffee, if I buy equipment.</li>
+  <li><span class='bold'>Cost per unit without equipment:</span> The $3 I need to spend for each cup of coffee I'd buy at Starbucks.</li>
+</ul>
 </p>
 """, width=PAGE_WIDTH)
 
